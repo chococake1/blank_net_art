3a0da175ca6242de0945770bad5ca873cff400b2
capogreco <thomas.capogreco@gmail.com>
2023-03-24 01:55:39 +1100

    removed pink square

--- public/sketch.js
+++ public/sketch.js
@@ -3,13 +3,8 @@ document.body.style.overflow = `hidden`
 
 function setup () {
    createCanvas (innerWidth, innerHeight)
-   rectMode (CENTER)
-   fill (`deeppink`)
-   noStroke ()
-   noLoop ()
 }
 
 function  draw () {
    background (`turquoise`)
-   square (width / 2, height / 2, 100)
 }
