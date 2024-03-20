c145180f4a25ed3ed5b8a8d89677af60b33bbb53
capo_greco <thomas.capogreco@gmail.com>
2024-03-20 16:15:27 +1100

    ditching p5

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.body.style.margin   = 0
+document.body.style.overflow = `hidden`
+
+const cnv = document.getElementById (`cnv_element`)
+cnv.width = innerWidth
+cnv.height = innerHeight
+
+const ctx = cnv.getContext (`2d`)
+ctx.fillStyle = `turquoise`
+ctx.fillRect (0, 0, innerWidth, innerHeight)
